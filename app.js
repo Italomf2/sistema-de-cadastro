b047bb9a3ae2d6babd5d4dce12b487bcaca79f05
Italomf2 <italomf44@gmail.com>
2023-09-20 21:23:44 -0300

    testando o comando git add para selecionar uma pasta modificada para dar commit somente nela

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("rodando o sistema de gerenciamento de jogos") 
 console.log("teste do git checkout ID da hash")
+
+console.log("só adicionando uma nova atualizacao para testar o git add, que serve para selecionar a pasta q foi modificada e assim eu poder commitar")
